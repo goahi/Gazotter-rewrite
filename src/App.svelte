e52e1c746c84fcc8b6d1e643d5794bb91648c58c
goahi <goai@protonmail.com>
2021-07-06 23:02:26 +0900

    add: add preview function

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
     import Header from "./Header.svelte";
 
-    let input_text;
+    let input_text = "";
+    let input_html;
+    $: input_html = input_text.split("\n");
 </script>
 
 <Header />
@@ -10,27 +12,37 @@
     <section class="input_area">
         <textarea bind:value={input_text} autocomplete="off" />
     </section>
-    <p>
-        {input_text}
-    </p>
+    <section class="preview">
+        {#each input_html as paragraph}
+            {paragraph}
+            <br />
+        {/each}
+    </section>
 </main>
 
 <style>
     main {
+        display: flex;
+
+        justify-content: space-around;
         margin-top: 3rem;
     }
 
     .input_area {
         display: flex;
-        width: 100%;
+        width: 45vw;
         padding-top: 1rem;
         flex-direction: column;
         align-items: center;
     }
 
     textarea {
-        width: 90vw;
-        height: 100px;
+        width: 100%;
+        height: 120px;
         resize: vertical;
+    }
+
+    .preview {
+        width: 45vw;
     }
 </style>
