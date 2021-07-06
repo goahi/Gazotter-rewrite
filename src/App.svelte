1ad479258361e2233336cb7108e3b5ed4a27d365
goahi <goai@protonmail.com>
2021-07-06 22:38:09 +0900

    add: add input box

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,36 @@
 <script>
     import Header from "./Header.svelte";
+
+    let input_text;
 </script>
 
 <Header />
 
-<main></main>
+<main>
+    <section class="input_area">
+        <textarea bind:value={input_text} autocomplete="off" />
+    </section>
+    <p>
+        {input_text}
+    </p>
+</main>
 
 <style>
+    main {
+        margin-top: 3rem;
+    }
+
+    .input_area {
+        display: flex;
+        width: 100%;
+        padding-top: 1rem;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    textarea {
+        width: 90vw;
+        height: 100px;
+        resize: vertical;
+    }
 </style>
