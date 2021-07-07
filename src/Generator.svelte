a3098a8d97fad87b0abc7b0bb9d4fd392073902b
goahi <goai@protonmail.com>
2021-07-07 18:52:40 +0900

    fix: fix linespacing

--- src/Generator.svelte
+++ src/Generator.svelte
@@ -10,15 +10,35 @@
 
     let dataURL;
 
+    let target;
+    let r_height;
+    let r_width;
+
+    if (c) {
+        ctx = c.getContext("2d");
+
+        ctx.textBaseline = "top";
+    }
+
     $: if (c) {
+        function measure(target_letters, font_family, font_size) {
+            target.style.fontFamily = font_family;
+            target.style.fontSize = `${font_size}px`;
+
+            target.textContent = target_letters;
+
+            return { height: target.offsetHeight, width: target.offsetWidth };
+        }
+
         ctx = c.getContext("2d");
 
         ctx.clearRect(0, 0, canvas_width, canvas_height);
-        ctx.textBaseline = "top";
         ctx.font = `${fontsize}px sans-serif`;
 
+        const text_height = measure(input[0], "sans-serif", fontsize).height;
+
         input.forEach((paragraph, order) => {
-            ctx.fillText(paragraph, 10, (order + 1) * 50);
+            ctx.fillText(paragraph, 10, (order + 1) * text_height);
         });
         dataURL = c.toDataURL();
     }
@@ -27,8 +47,15 @@
 <img src={dataURL} alt={input.join("")} />
 <canvas bind:this={c} height={canvas_height} width={canvas_width} />
 
+<span id="ruler" bind:this={target} />
+
 <style>
     canvas {
         display: none;
+    }
+
+    #ruler {
+        white-space: nowrap;
+        visibility: hidden;
     }
 </style>
